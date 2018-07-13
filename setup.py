e77f27c3929e48ebe67770cb230668de0928fd03
Tracy Poff <tracy.poff@gmail.com>
2018-07-13 00:46:19 -0400

    Update setup. Add .travis.yml.

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 from setuptools import setup
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(name='vidua',
       description='Apply patches to ROMs and other files',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='http://github.com/sopoforic/vidua',
       author='Tracy Poff',
       author_email='tracy.poff@gmail.com',
       classifiers=[
-          'Development Status :: 2 - Pre-Alpha',
+          'Development Status :: 3 - Alpha',
           'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3',
           'Topic :: Utilities',
       ],
       packages=['vidua'],
-      setup_requires=['setuptools_scm'],
+      setup_requires=['setuptools_scm', 'pytest-runner'],
+      tests_require=['pytest'],
       use_scm_version=True,
       zip_safe=True)
