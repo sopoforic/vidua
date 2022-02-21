76604ca58e0c670eeef70e6567ae6fe7efa516e1
Tracy Poff <tracy.poff@gmail.com>
2022-02-21 02:34:10 -0500

    Test identifying non-patch file.

--- vidua/test/test_identify.py
+++ vidua/test/test_identify.py
@@ -11,6 +11,7 @@ test_files = [
     ('test_ips/test_b.ips', PatchType.IPS),
     ('test_bps/test_a.bps', PatchType.BPS),
     ('test_bps/test_b.bps', PatchType.BPS),
+    ('__init__.py', None),
 ]
 
 test_files = [(os.path.join(BASE_PATH, f[0]), f[1]) for f in test_files]
